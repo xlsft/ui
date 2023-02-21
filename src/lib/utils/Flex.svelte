f5770ba6090e74f54056ffb40b0c94f29d519c7b
TypoStudio <typostudio48@gmail.com>
2023-02-21 15:16:09 +0300

    first commit

---
+++ src/lib/utils/Flex.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export let width = "100%", heigth = "100%", direction = "column", justify = "center", align = "center", wrap = "nowrap", aligncont="center", gap = "10px"
+    let flex = `
+    width: ${width}; 
+    height: ${heigth}; 
+    display: flex; 
+    flex-direction: ${direction}
+    flex-wrap: ${wrap};
+    justify-content: ${justify};
+	align-items: ${align};
+    align-content: ${aligncont};
+    gap: ${gap}
+    `
+</script>
+
+<div class="xl-ui-flex" style="{flex}">
+    <slot></slot>
+</div>
+
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Flex from "$lib/utils/Flex.svelte";
+    import Main from "$lib/utils/Main.svelte";
+
+</script>
+<Main>
+    <Flex>
+        <slot></slot>
+    </Flex>
+</Main>
